7e49ff1e9197b05b9b7ba75f7c1b2177413610a1
mck0324 <dch3030@naver.com>
2026-01-31 14:25:38 +0900

    modify : response, request schema

--- app/models/schemas.py
+++ app/models/schemas.py
@@ -1,12 +1,12 @@
 from pydantic import BaseModel
 
 class QueryResponse(BaseModel):
-    question: str
-
-class QueryRequest(BaseModel):
     market: str
     symbol: str
     name: str
     date: str
     price_type: str
     value: float
+
+class QueryRequest(BaseModel):
+    question: str
